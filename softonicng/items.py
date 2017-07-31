cf854c1dced69a5f53b1e59a50be29c9b1cb4570
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-07-30 20:23:42 -0400

    First commit

---
+++ softonicng/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class SoftonicngItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    file_urls = scrapy.Field()
+    files = scrapy.Field() 
+    title = scrapy.Field()
+    os = scrapy.Field()
+    description = scrapy.Field()
+
---
+++ softonicng/spiders/windsoft.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from softonicng.items import SoftonicngItem
+
+
+class WindsoftSpider(scrapy.Spider):
+    name = 'windsoft'
+    allowed_domains = ['en.softonic.com']
+    start_urls = ['http://en.softonic.com/windows/best-software']
+
+    def parse(self, response):
+    	IND = 1
+    	for item in response.xpath('//li[@class=$val]', val='list-program-item js-listed-program'):
+    		strpath = '//*[@id="program_list"]/li[{:d}]/a/@href'.format(IND)
+    		path = item.xpath(strpath).extract_first()
+    		yield scrapy.Request(path + '/download', callback = self.parse_download)
+    		yield scrapy.Request(path, callback = self.parse_deatils)
+    		IND += 1
+
+    	next_page = response.css('a.pagination-next ::attr(href)').extract_first()
+    	if next_page:
+    		yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+    def parse_deatils(self, response):
+    	name = response.xpath('//h1[@class=$val]/text()', val='media-app__title').extract_first()
+    	vers = response.xpath('//h1[@class=$val]/span/text()', val='media-app__title').extract_first()
+    	try:
+    		parse_title = repr('{:s} {:s}'.format(name, vers))
+    	except:
+    		parse_title = repr('{:s}'.format(name))
+    	parse_description = repr(response.xpath('normalize-space(.//*[@id="app-softonic-review"]/article)').extract_first())
+    	parse_os = repr(response.xpath('normalize-space(.//p[@itemprop=$val]/text())', val='operatingSystem').extract_first())
+    	screenshots = response.xpath('//a[@class=$val]/@href', val='gallery__media-links').extract()
+    	yield SoftonicngItem(
+    		image_urls = screenshots,
+    		title = parse_title,
+    		os = parse_os,
+    		description = parse_description,
+    	)
+
+    def parse_download(self, response):
+    	pathto = '//*[@id="js-app-download-info"]/a/@href'
+    	valto = 'btn btn--alternative btn--small js-alternative-button' 
+    	downfile = response.xpath(pathto, val=valto).extract()
+    	if downfile:
+    		yield SoftonicngItem(
+    			file_urls = downfile
+    		)
+    	else:
+    		self.logger.debug('[>> NOT URL to download <<]')
