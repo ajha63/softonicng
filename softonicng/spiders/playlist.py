f64e28889517a7b988cf15dee69a238ba64266db
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-09-27 17:54:27 -0400

    playlist.py it's work

--- softonicng/spiders/playlist.py
+++ softonicng/spiders/playlist.py
@@ -2,25 +2,32 @@
 import scrapy
 import pprint
 from softonicng.items import DownloadngItem
-from softonicng.items import FileItem
 
 
 class WindsoftSpider(scrapy.Spider):
-    name = 'cnetwind'
-    allowed_domains = ['download.cnet.com']
-    start_urls = ['http://download.cnet.com/most-popular/windows/']
-    def parse(self, response):
-    	IND = 1
-    	for item in response.xpath('//*[@id="search-results"]/a'):
-    		pathToDetails = '//*[@id="search-results"]/a[{:d}]/@href'.format(IND)
-    		itemDetails = item.xpath(pathToDetails).extract_first()
-    		# remove all self.logger.debug
-    		self.logger.debug(itemDetails)
-    		IND += 1
+	name = 'cnetwind'
+	allowed_domains = ['download.cnet.com']
+	start_urls = ['http://download.cnet.com/most-popular/windows/']
+	def parse(self, response):
+		IND = 1
+		for item in response.xpath('//*[@id="search-results"]/a'):
+			pathToDetails = '//*[@id="search-results"]/a[{:d}]/@href'.format(IND)
+			itemDetails = item.xpath(pathToDetails).extract_first()
+			IND += 1
+			yield scrapy.Request(itemDetails, callback = self.parse_package)
 
-    	next_page = response.css('a.next ::attr(href)').extract_first()
-    	if next_page:
-    		yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+		next_page = response.css('a.next ::attr(href)').extract_first()
+		if next_page:
+			yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
-    def parse_package(self, response):
-    	pass
+	def parse_package(self, response):
+		img = response.xpath('//*[@id="content-body-product-single"]/header/h1/span[1]/img/@src')
+		imageURL = [img.extract_first()]
+		screenShots = '//*[@id="product-screenshots"]/div[2]/div/ul/li'
+		for image in response.xpath(screenShots):
+			imageSrc = image.css('img.om-image-view  ::attr(data-large-img)').extract_first()
+			imageURL.append(imageSrc)
+
+		yield DownloadngItem(
+			image_urls = imageURL
+		)
